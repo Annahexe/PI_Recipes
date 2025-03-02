c585e5d49290bc17fbc17213513cd1b346043b2f
Annahexe <annamariamarinoco@gmail.com>
2025-03-02 18:14:56 +0100

    + Index HTML, Forms (POST Routes)

--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 //Consultar el archivo README para más información sobre los módulos.
 
 console.log("Annamaria Marino | 2024 | Florida Universitària \n");
-console.log(
-  "Ejercicio de configuracion prototipo de servidor (PI_005_Prototipo)"
-);
+console.log("Ejercicio de configuracion prototipo de servidor (PI_005_Prototipo)");
 
 //npm run dev
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
 
+//Middleware
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+
+//Settings Puerto
 app.set("port", process.env.PORT || 8080);
 
-app.use(morgan("dev"));
 //Routes
 app.use(require("./routes/index"));
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,27 +5,47 @@ const router = Router();
 //localhost:8080/
 
 // Carga JSON y Recetas
-let recipes;
+let recipesData;
 
 const loadRecipes = () => {
   let rawdata = fs.readFileSync("src/recipes.json");
-  recipes = JSON.parse(rawdata);
+  recipesData = JSON.parse(rawdata);
 };
 loadRecipes();
 
-//Rutas GET
 router.get("/", (req, res) => {
   console.log("Alguien ha accedido al servidor");
   res.send(
-    "Bienvenido al servidor, puedes acceder a las siguientes rutas:\n RUTAS GET: /about; /rutaRecetas; /rutaPizza; /rutaVegetarian; /rutaTomatoSauce\n RUTAS POST: /rutaPOST; /formulario; /agregarReceta; /buscarReceta; /filtrarPorIngrediente"
+    "Bienvenido al servidor, puedes acceder a las siguientes rutas:\n RUTAS GET: /about; /rutaRecetas; /rutaPizza; /rutaVegetarian; /rutaTomatoSauce\n RUTAS POST: /rutaPOST; /buscarReceta; /filtrarPorIngrediente"
   );
 });
 
+//RUTAS POST CON HTML
+router.post("/formulario", (req, res) => {
+  console.log("Enviada peticion POST /formulario.");
+  console.log(req.body);
+  console.log(req.body.nombre);
+  console.log(req.body.apellido);
+  console.log(req.body.email);
+  res.redirect("http://127.0.0.1:5500/src/index.html");
+});
+
+router.post("/addRecipe", (req, res) => {
+  console.log("Alguien ha intentado agregar una nueva receta.");
+  console.log(req.body);
+  res.json({
+    name: req.body.name,
+    ingredients: req.body.ingredients,
+    preference: req.body.preference,
+    description: req.body.description,
+  });
+});
+
+//RUTAS GET
+
 router.get("/about", (req, res) => {
   console.log("Alguien ha accedido a la ruta About");
-  res.send(
-    "Prototipo de ListOChef para consultar recetas y manejar inventario."
-  );
+  res.send("Prototipo de ListOChef para consultar recetas y manejar inventario.");
 });
 
 router.get("/rutaRecetas", (req, res) => {
@@ -37,9 +57,7 @@ router.get("/rutaPizza", (req, res) => {
   console.log("Alguien ha accedido a la ruta Pizza");
 
   //find necesita una funcion como parametro
-  const response = recipes.recipes.find(
-    (recipe) => recipe.name === "Classic Margherita Pizza"
-  );
+  const response = recipesData.recipes.find((recipe) => recipe.name === "Classic Margherita Pizza");
   res.json(response);
 });
 
@@ -47,16 +65,14 @@ router.get("/rutaVegetarian", (req, res) => {
   console.log("Alguien ha accedido a la ruta Vegetarian");
 
   //filter funciona para multiples resultados
-  const response = recipes.recipes.filter(
-    (recipe) => recipe.preference === "vegetarian"
-  );
+  const response = recipesData.recipes.filter((recipe) => recipe.preference === "vegetarian");
   res.json(response);
 });
 
 router.get("/rutaTomatoSauce", (req, res) => {
   console.log("Alguien ha accedido a la ruta Tomato Sauce");
 
-  const response = recipes.recipes.filter(
+  const response = recipesData.recipes.filter(
     (recipe) => recipe.ingredients.includes("tomato sauce")
     //utilizamos includes para el array ingredients
   );
@@ -65,48 +81,12 @@ router.get("/rutaTomatoSauce", (req, res) => {
 
 //POST
 
-router.post("/rutaPOST", (req, res) => {
-  console.log("Alguien ha accedido a la ruta POST.");
-  res.send("hola haciendo pruebas con la ruta POST.");
-});
-
-router.post("/formulario", (req, res) => {
-  req.body =
-    "En el req body guardaremos los datos del formulario para enviarlos a la base de datos (ej. registro)";
-  console.log("Alguien ha accedido al formulario.");
-  res.json({ nombre: "Cosas POST para un formulario." });
-});
-
-router.post("/agregarReceta", (req, res) => {
-  req.body =
-    "En el req body guardaremos los datos de nuevas recetas que el usuario quiere agregar";
-  console.log("Alguien ha intentado agregar una nueva receta.");
-  res.json({ mensaje: "Receta agregada." });
-});
-
-router.post("/buscarReceta", (req, res) => {
-  console.log("Alguien esta buscando una receta.");
-  //En el req body guardaremos el nombre de la receta para buscar
-  req.body = "Classic Margherita Pizza";
-  const nombre = req.body;
-  const recetaEncontrada = recipes.recipes.find(
-    (recipe) => recipe.name === nombre
-  );
-  if (recetaEncontrada) {
-    res.json(recetaEncontrada);
-  } else {
-    res.send("Receta no encontrada");
-  }
-});
-
 router.post("/filtrarPorIngrediente", (req, res) => {
   console.log("Alguien esta filtrando por ingrediente.");
   //En el req body guardaremos los filtros a aplicar
   req.body = "parmesan cheese";
   const ingrediente = req.body;
-  const filtroAplicado = recipes.recipes.filter((recipe) =>
-    recipe.ingredients.includes(ingrediente)
-  );
+  const filtroAplicado = recipesData.recipes.filter((recipe) => recipe.ingredients.includes(ingrediente));
   res.json(filtroAplicado);
 });
 
